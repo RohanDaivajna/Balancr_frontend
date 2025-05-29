e870bf946cdc3fcfcd90fdc704b2f0109bc33b78
Rohan Daivajna <rohan11daivajna@gmail.com>
2025-05-29 19:56:02 +0530

    changes

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
+// filepath: c:\Users\rohan\Documents\GitHub\Balancr_frontend\postcss.config.cjs
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 }
